1f7535ae8ac2a19417065e0c5553c156e5808025
Fitranda <ramadhana689@gmail.com>
2020-09-26 14:01:18 +0700

    Minggu ke-15
    Tugas PHP SMK Video 46-50 , Minggu ke-15

--- restoran/home/beli.php
+++ restoran/home/beli.php
@@ -4,7 +4,7 @@
     if (isset($_GET['hapus'])) {
         $id=$_GET['hapus'];
         unset($_SESSION['_'.$id]);
-        
+        header("location:?f=home&m=beli");
     }
 
     if (isset($_GET['tambah'])) {
@@ -49,6 +49,7 @@
 
         global $db;
         $total=0;
+        global $total;
 
         echo '        
         <table class="table table-bordered w-70">
@@ -96,6 +97,24 @@
         </tr>';
         echo '</table>';
     }
+
+
+
+?>
+<?php 
+
+    if (!empty($total)) {
+
+
+?>
+
+
+<a class="btn btn-primary" href="?f=home&m=checkout&total=<?php echo $total; ?>" role="button">CHECKOUT</a>
+
+<?php     
+
+    }
+
 
 
 ?>
